a38a9ae0625acb4065b7eb296c812ff0ca6e5463
Clown <1104480806@qq.com>
2021-05-12 13:29:20 +0800

    create SongList.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,75 @@
 <template>
   <div id="app">
-
+    <div class="portrait">
+      <img :src="image" alt="歌手肖像"/>
+    </div>
+    <SongList :data="dataSource"></SongList>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import SongList from '@/components/SongList'
 
+export default {
+  components: {
+    SongList
+  },
+  data() {
+    return {
+      dataSource: [],
+      image: null
+    }
+  }
+  ,
+  created() {
+    axios
+        .get('http://localhost:3000/artists?id=3684')
+        .then((data) => {
+          const img = data.data.artist.picUrl
+          const newData = data.data.hotSongs.map((item) => ({
+            id: item.id,
+            name: item.name,
+            ar: item.ar.map((arItem) => arItem.name).join('/'),
+            al: {
+              name: item.al.name,
+              picUrl: item.al.picUrl,
+            },
+          }))
+          this.dataSource = newData
+          this.image = img
+        })
+        .catch((error) => console.log(error))
+  }
+}
 </script>
 
 <style lang="scss">
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+li {
+  list-style: none;
+}
+
+.portrait {
+  width: 100vw;
+  height: 60vh;
+
+  img {
+    width: 100%;
+  }
+}
 </style>
---
+++ src/components/SongList.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="list-wrapper">
+    <ul>
+      <li
+          class="item"
+          v-for="(item, index) in data"
+          :key="item.id"
+          @click="songClick(item)"
+      >
+        <p class="index">{{ index + 1 }}</p>
+        <div class="content">
+          <p class="name">{{ item.name }}</p>
+          <p class="info">
+            <span>
+              {{ item.ar }}
+            </span>
+            <span> - </span>
+            <span>{{ item.al.name }}</span>
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+
+export default {
+  props: {
+    data: Array
+  },
+}
+</script>
+<style lang="scss" scoped>
+.list-wrapper {
+  width: 100vw;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  height: 50vh;
+  overflow: auto;
+  position: absolute;
+  left: 0;
+  bottom: 60px;
+  background: white;
+
+  .item {
+    display: flex;
+    padding: 10px;
+
+    > .index {
+      width: 40px;
+      height: 40px;
+      text-align: center;
+      line-height: 40px;
+      font-size: 16px;
+    }
+
+    > .content {
+      flex: 1;
+      height: 40px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+
+      > .name {
+        font-size: 16px;
+        font-weight: normal;
+      }
+
+      > .info {
+        font-size: 14px;
+        color: #555;
+      }
+    }
+  }
+}
+</style>
